087d0004438d5d29c4d7c21c0d95ed1eb06d65fa
白芸实 <420424306@qq.com>
2020-08-03 15:59:26 +0800

    init

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *@ClassName DemoController
+ *@Description
+ *@Author 白云
+ *@Date 2020/7/2
+ **/
+
+@RestController
+@RequestMapping("/demo")
+@RefreshScope
+public class DemoController {
+
+    @Value("${spring.name}")
+    private String name;
+
+    @GetMapping("/hello")
+    public String demo(){
+        return "hello world:" + name;
+    }
+}
